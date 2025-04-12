f6c7156929243a39b592e55e4c3362322bf23163
Arhaan Siddiquee <siddiqueearhaan@gmail.com>
2025-04-12 13:09:52 +0530

    favicon

--- rim/src/App.jsx
+++ rim/src/App.jsx
@@ -1,5 +1,4 @@
-// App.jsx
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/home"; 
 import Latex from "./components/latex"; 
 
